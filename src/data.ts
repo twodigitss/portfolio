283ebaed2d8fbb976f03252b1c9cfb8217e02ab7
twodigitss <100657622+twodigitss@users.noreply.github.com>
2026-07-26 10:15:05 -0600

    hey! se me ocurrio el sidebar negro

---
+++ src/data.ts
@@ -0,0 +1,12 @@
+import { parse } from 'yaml';
+
+export async function AboutMe(){
+  const CvData = await fetch(
+    "https://gist.githubusercontent.com/twodigitss/fdd80b8202366fb4454f4bb470416ff1/raw/cv.txt",
+  ).then((res) => res.text()).then((text) => parse(text).cv);
+
+  const experience = CvData.sections.experience;
+  return experience
+}
+
+
