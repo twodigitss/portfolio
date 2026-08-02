38fe80d82cb609abae16a4350a70d4af6f7f5ed0
twodigitss <100657622+twodigitss@users.noreply.github.com>
2026-08-02 08:15:17 -0600

    bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
 site: 'https://twodigitss.github.io',
-base: '/portfolio',
 vite: {
     plugins: [tailwindcss()],
   },
